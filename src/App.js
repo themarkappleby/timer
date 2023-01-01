f66b3ce03cac7cc2c3c6e28e865af25303af0a25
Mark Appleby <mark.appleby@shopify.com>
2023-01-01 15:39:21 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const sfx = new Audio('/boxing-bell.mp3');
+const TWEAK = 5;
+let interval = null;
+
+async function preventScreenDim () {
+  if ('wakeLock' in navigator) {
+    let wakeLock = null;
+    try {
+      wakeLock = await navigator.wakeLock.request('screen');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+preventScreenDim();
+
+const getProgress = (current, total) => {
+  const progress = (total - current) / total * 100;
+  return `${progress}%`;
+}
+
 function App() {
+  const [startTime, setStartTime] = useState(60);
+  const [seconds, setSeconds] = useState(startTime);
+  const [running, setRunning] = useState(false);
+
+  const startTimer = () => {
+    if (running) {
+      setRunning(false);
+      setSeconds(startTime);
+      clearInterval(interval);
+    } else {
+      setRunning(true);
+      interval = setInterval(() => {
+        setSeconds(seconds => {
+          const newSeconds = seconds - 1;
+          if (newSeconds < 0) {
+            sfx.play();
+            setRunning(false);
+            setSeconds(startTime);
+            clearInterval(interval);
+            return 0;
+          }
+          return newSeconds;
+        });
+      }, 1000);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="progress" style={{height: getProgress(seconds, startTime)}} />
+      <div className="time">
+        {seconds}
+      </div>
+      <button onClick={startTimer} className="touchTarget" />
+      { !running && (
+        <div className="controls">
+          <button onClick={() => {
+            if (startTime > 5) {
+              setSeconds(startTime - TWEAK)
+              setStartTime(startTime - TWEAK)
+            }
+          }}>-</button>
+          <button onClick={() => {
+            setSeconds(startTime + TWEAK)
+            setStartTime(startTime + TWEAK)
+          }}>+</button>
+        </div>
+      )}
     </div>
   );
 }
